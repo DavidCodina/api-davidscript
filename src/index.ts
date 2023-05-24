cbfbe101ae3bfc42515d955a11a7d1636c8a731c
David Codina <davidmcodina@icloud.com>
2023-05-24 13:35:30 -0600

    push to main test 1

--- src/index.ts
+++ src/index.ts
@@ -103,7 +103,7 @@ app.get('/', (req, res) => {
   const body = req.body || {}
 
   return res.status(200).json({
-    message: 'if condition test 4',
+    message: 'push test 1',
     envTest: process.env.TEST,
     body: body
   })
