7be8cc7ca334999d267b92389eb96f99c71d765f
David Codina <davidmcodina@icloud.com>
2023-05-25 08:31:15 -0600

    env test

--- src/index.ts
+++ src/index.ts
@@ -103,8 +103,9 @@ app.get('/', (req, res) => {
   const body = req.body || {}
 
   return res.status(200).json({
-    message: 'Hello Chris!',
+    message: 'Hello Dave!',
     envTest: process.env.TEST,
+    date: process.env.DATE,
     body: body
   })
 })
